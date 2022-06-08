d96dedfdbc2ab7c0ab3c3e099a011c356683d783
ᴹᴿᤎ KhairilOfcི ࿇ <90993998+Khairil-Fnt@users.noreply.github.com>
2022-06-08 12:14:41 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@
 
   // Edit Sesuai Kemauanmu
   global.autoReadGc = true // Ubah Ke false Jika Chat Di Grup Tidak Ingin Di Baca Oleh Bot
-  global.autoReadAll = false // Ubah Ke true Jika Ingin Seluruh Chat Di Baca Oleh Bot
+  global.autoReadAll = true // Ubah Ke true Jika Ingin Seluruh Chat Di Baca Oleh Bot
   global.anticall = true // Ubah Ke false Jika Tidak Ingin Bot Blockir Yang Telepon
   global.available = false // Ubah Ke false Jika Bot Mu Tidak Ingin Terlihat Online
   global.autoTyping = false // Ubah Ke true Jika Ingin Saat Ada Yang Chat Bot Auto Mengetik
@@ -23,18 +23,18 @@
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
-  global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
-  global.ownernomer = '6281575886399' // Owner Utama Istilahnya
-  global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
-  global.packname = 'Hinata Hyuga' // Sticker Weem
-  global.author = 'Riy' // Sticker Weem
-  global.webme = 'https://api-riy.herokuapp.com' // Ganti Sama Web Mu
-  global.linkgroup = 'https://chat.whatsapp.com/HZhLynp6E9QKNrRFtUp26T' // Ganti Sama Link Gc Mu
+  global.ownername = 'Khairil春' // Ganti Jadi Namamu
+  global.owner = ['60109102984','601124024797','60109102984'] // Kalo Mau Lebih Tambahin Aja
+  global.ownernomer = '60109102984' // Owner Utama Istilahnya
+  global.botname = 'BebanBot天' // Ganti Jadi Nama Botmu
+  global.packname = 'BebanBot天' // Sticker Weem
+  global.author = 'Khairil春' // Sticker Weem
+  global.webme = 'https://islamonline.net/en/home/?amp' // Ganti Sama Web Mu
+  global.linkgroup = 'https://chat.whatsapp.com/BL8CTzUqg9KFJ0Mu6GmUvs' // Ganti Sama Link Gc Mu
   global.sessionName = 'session'
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw' // Ganti Sama Link Yt Mu
-  global.github = 'https://github.com/riychdwayne' // Ganti Sama Link Github Mu
-  global.instagram = 'https://instagram.com/riychdwayne' // Ganti Sama Link Ig mu
+  global.youtube = 'https://youtube.com/channel/UCnVOCp4m5aj6BKDktn0GUxQ' // Ganti Sama Link Yt Mu
+  global.github = 'https://github.com/KhairilOFC' // Ganti Sama Link Github Mu
+  global.instagram = 'https://instagram.com/bangkelah_71' // Ganti Sama Link Ig mu
 
   // Background Setiap Menu
   global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -62,7 +62,7 @@
   group: '_Khusus Group 😏_',
   private: '_Khusus Private Chat_',
   bot: '_Khusus Pengguna Nomer Bot_',
-  wait: '_Sabar Sedang Di Proses ..._',
+  wait: 'Sabar Loading! Orang Sabar Disayang Janda...',
   done: '_Done Ngab 🤗_',
   }
 
